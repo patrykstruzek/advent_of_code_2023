525c5844c498c22e000a1c2e68d28b8fe51d8a7a
Patryk Strużek <patrykstruzek17@gmail.com>
2023-12-01 17:44:42 +0100

    Add incorrect day01 part2 and improve some code.

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,14 +1,17 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <stdexcept>
 #include <string>
+#include <string_view>
+#include <unordered_map>
 #include <vector>
 
-auto read_inputs(std::string filename) -> std::vector<std::string>
+auto read_inputs(const std::string& filename) -> std::vector<std::string>
 {
     std::ifstream file {filename};
     if (!file)
     {
-        throw std::runtime_error("Something goes wrong with reading the file!");
+        throw std::runtime_error("File reading failure!");
     }
 
     std::vector<std::string> inputs {};
@@ -17,44 +20,85 @@ auto read_inputs(std::string filename) -> std::vector<std::string>
     {
         inputs.push_back(line);
     }
-    return inputs;
+    return std::move(inputs);
 }
 
-auto seek_for_calibration_value(std::string line) -> int
+auto seek_for_cvalue_1(const std::string& line) -> int
 {
     std::vector<char> values {};
-    for (auto c : line)
+    for (const auto& c : line)
     {
         if (std::isdigit(c))
         {
             values.push_back(c);
         }
     }
-    auto value = std::string{} +  values.front() + values.back();
-    return std::stoi(value);
+    auto value = std::string {} + values.front() + values.back();
+    return std::stoi(std::move(value));
 }
 
-int main(int argc, char *argv[])
+auto seek_for_cvalue_2(const std::string& line) -> int
 {
-    if(argc > 2 || argc < 2)
+    std::unordered_map<std::string, char> numbers = {
+        {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
+        {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9},
+    };
+
+    std::vector<char> values {};
+    for (const auto& number : numbers)
     {
-        std::cerr << "Invalid amount of args! You can only pass one file.";
+        auto x = line.find(number.first);
+        if (x != std::string::npos)
+        {
+            values.push_back(number.second);
+        }
+    }
+    return values.front() * 10 + values.back();
+}
+
+auto run(const std::string& filename, std::string_view option) -> bool
+{
+    int result = 0;
+    auto inputs = read_inputs(filename);
+    if (option == "-p1")
+    {
+        for (const auto& line : inputs)
+        {
+            result += seek_for_cvalue_1(line);
+        }
+    }
+    else if (option == "-p2")
+    {
+        for (const auto& line : inputs)
+        {
+            result += seek_for_cvalue_2(line);
+        }
+    }
+    else
+    {
+        std::cerr << "Invalid arg!";
+        return false;
+    }
+    std::cout << "result: " << result;
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc > 3 || argc < 3)
+    {
+        std::cerr << "Invalid amount of args! You can only pass one file and one arg.";
         return 1;
     }
-    std::string filename = argv[1];
-
+    std::string filename = argv[2];
+    std::string option = argv[1];
     try
     {
-        int result = 0;
-        auto inputs = read_inputs(filename);
-        for (auto line : inputs)
-        {
-            result += seek_for_calibration_value(line);
-        }
-        std::cout << "result: " << result;
+        return run(filename, option);
     }
-    catch (const std::exception &e)
+    catch (const std::exception& e)
     {
         std::cerr << e.what() << std::endl;
+        return 1;
     }
 }
