4f2f3c5edb015379b24d5f367aae3356240569b2
Patryk Strużek <patrykstruzek17@gmail.com>
2023-12-01 23:08:40 +0100

    Second wrong way to solve day01 part2

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,12 +1,15 @@
+#include <vcruntime.h>
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
-auto read_inputs(const std::string& filename) -> std::vector<std::string>
+static auto read_inputs(const std::string& filename) -> std::vector<std::string>
 {
     std::ifstream file {filename};
     if (!file)
@@ -23,7 +26,8 @@ auto read_inputs(const std::string& filename) -> std::vector<std::string>
     return std::move(inputs);
 }
 
-auto seek_for_cvalue_1(const std::string& line) -> int
+// part 1
+static auto seek_for_cvalue_1(const std::string& line) -> int
 {
     std::vector<char> values {};
     for (const auto& c : line)
@@ -37,28 +41,41 @@ auto seek_for_cvalue_1(const std::string& line) -> int
     return std::stoi(std::move(value));
 }
 
-auto seek_for_cvalue_2(const std::string& line) -> int
+// part 2
+static auto seek_for_cvalue_2(const std::string& line) -> int
 {
-    std::unordered_map<std::string, char> numbers = {
+    const std::unordered_map<std::string, char> numbers = {
         {"one", 1}, {"two", 2},   {"three", 3}, {"four", 4}, {"five", 5},
         {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9},
     };
 
-    std::vector<char> values {};
-    for (const auto& number : numbers)
+    auto result = std::pair<int, int> {};
+
+    size_t lborder_distance {};
+    size_t rborder_distance {};
+    for (const auto& [alpha, digit] : numbers)
     {
-        auto x = line.find(number.first);
-        if (x != std::string::npos)
+        size_t pos = line.find(alpha);
+        if (pos != std::string::npos && pos < lborder_distance)
         {
-            values.push_back(number.second);
+            lborder_distance = pos;
+            result.first = digit;
+        }
+
+        pos = line.rfind(alpha);
+        if (pos != std::string::npos && pos > lborder_distance)
+        {
+            rborder_distance = pos;
+            result.second = digit;
         }
     }
-    return values.front() * 10 + values.back();
+
+    return 10 * result.first + result.second;
 }
 
-auto run(const std::string& filename, std::string_view option) -> bool
+static auto run(const std::string& filename, std::string_view option) -> bool
 {
-    int result = 0;
+    int result {};
     auto inputs = read_inputs(filename);
     if (option == "-p1")
     {
@@ -92,6 +109,7 @@ int main(int argc, char* argv[])
     }
     std::string filename = argv[2];
     std::string option = argv[1];
+
     try
     {
         return run(filename, option);
