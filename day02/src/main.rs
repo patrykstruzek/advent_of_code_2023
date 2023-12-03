28aac0d32d53600621e73113994ffc16f30a20e5
Patryk Strużek <patrykstruzek17@gmail.com>
2023-12-03 18:50:23 +0100

    Add day02 part2
    There is a lot of a code repetition, but I don't have a time to fix it.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,5 @@
+// I know that this is not the best solution, among other things, because there is a lot of code repetition,
+// but it is what it is. Maybe I'll fix it in the future.
 use lazy_static::lazy_static;
 use std::env;
 use std::{
@@ -11,13 +13,66 @@ lazy_static! {
 }
 
 fn main() {
-    let filepath = get_args();
+    let (filepath, option) = get_args();
     let mut inputs = get_inputs(&filepath);
-    let result = calc_inputs(&mut inputs);
-    print!("result: {}", { result });
+    if option == "-p1" {
+        let result = calc_inputs1(&mut inputs);
+        print!("result: {}", { result });
+    } else if option == "-p2" {
+        let result = calc_inputs2(&mut inputs);
+        print!("result: {}", { result });
+    } else {
+        panic!("Bad args!");
+    }
 }
 
-fn calc_inputs(inputs: &mut Vec<String>) -> u32 {
+fn calc_inputs2(inputs: &mut Vec<String>) -> u32 {
+    let mut result = 0;
+    for (n, line) in inputs.iter_mut().enumerate() {
+        println!("game {}", n + 1);
+        result += get_set_power(line);
+    }
+    return result;
+}
+
+fn get_set_power(line: &mut str) -> u32 {
+    let (id, line_) = line.split_once(": ").unwrap();
+    let sets: Vec<&str> = line_.split("; ").collect();
+    let cubes: Vec<(&str, u32)> = sets
+        .iter()
+        .flat_map(|&l| {
+            l.split(';').flat_map(|set| {
+                set.split(',').map(|pair| {
+                    let mut parts = pair.trim().splitn(2, ' ');
+                    let quantity = parts.next().unwrap().parse::<u32>().unwrap();
+                    let color = parts.next().unwrap();
+                    (color, quantity)
+                })
+            })
+        })
+        .collect();
+
+    let (mut fred, mut fgreen, mut fblue) = (0, 0, 0);
+    for (color, value) in cubes.iter() {
+        if color == &"red" {
+            if *value > fred {
+                fred = *value;
+            }
+        } else if color == &"green" {
+            if *value > fgreen {
+                fgreen = *value;
+            }
+        } else if color == &"blue" {
+            if *value > fblue {
+                fblue = *value;
+            }
+        }
+    }
+
+    return fred * fgreen * fblue;
+}
+
+fn calc_inputs1(inputs: &mut Vec<String>) -> u32 {
     let mut result = 0;
     for (n, line) in inputs.iter_mut().enumerate() {
         println!("game {}", n + 1);
@@ -72,10 +127,10 @@ fn get_inputs(path: &str) -> Vec<String> {
     buffer.lines().map(move |l| l.unwrap()).collect()
 }
 
-fn get_args() -> String {
+fn get_args() -> (String, String) {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 2 {
-        return args[1].clone();
+    if args.len() == 3 {
+        return (args[1].clone(), args[2].clone());
     }
     panic!("Too few args!");
 }
