16fbf725b6fb141d2dd5d30b2f3564af83957b83
Patryk Strużek <patrykstruzek17@gmail.com>
2023-12-23 22:24:53 +0100

    Code cleaning

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -42,11 +42,11 @@ fn get_set_power(line: &mut str) -> u32 {
             if *value > fred {
                 fred = *value;
             }
-        } else if color == &"green" {
+        } else if *color == "green" {
             if *value > fgreen {
                 fgreen = *value;
             }
-        } else if color == &"blue" {
+        } else if *color == "blue" {
             if *value > fblue {
                 fblue = *value;
             }
@@ -57,22 +57,17 @@ fn get_set_power(line: &mut str) -> u32 {
 }
 
 fn calc_inputs1(inputs: &mut Vec<String>) -> u32 {
-    let mut result = 0;
-    for line in inputs.iter_mut() {
-        result += get_id(line);
-    }
-    result
+    inputs.iter().map(|line| get_id(line.clone().as_mut_str())).sum()
 }
 
 fn get_id(line: &mut str) -> u32 {
     let (id, cubes) = parse_bag(line);
 
-    let mut all_correct = true;
-    for (color, value) in cubes.iter() {
-        if let Some(&bag_value) = CUBES_IN_BAG.get(color) {
-            all_correct &= *value <= bag_value;
-        }
-    }
+    let all_correct = cubes.iter().all(|(color, value)| {
+        CUBES_IN_BAG
+            .get(color)
+            .map_or(true, |bag_value| *value <= *bag_value)
+    });
 
     if all_correct {
         return id;
@@ -83,20 +78,24 @@ fn get_id(line: &mut str) -> u32 {
 fn parse_bag(line: &mut str) -> (u32, Vec<(&str, u32)>) {
     let (id, line_) = line.split_once(": ").unwrap();
 
-    let id_ = id.get(5..id.len()).unwrap().trim().parse::<u32>().unwrap();
-    let sets: Vec<&str> = line_.split("; ").collect();
-    let cubes: Vec<(&str, u32)> = sets
-        .iter()
-        .flat_map(|&l| {
+    let id_ = id[5..id.len()].trim().parse::<u32>().unwrap();
+
+    let cubes: Vec<(&str, u32)> = line_
+        .split("; ")
+        .flat_map(|l| {
             l.split(';').flat_map(|set| {
                 set.split(',').map(|pair| {
                     let mut parts = pair.trim().splitn(2, ' ');
-                    let quantity = parts.next().unwrap().parse::<u32>().unwrap();
-                    let color = parts.next().unwrap();
-                    (color, quantity)
+                    if let (Some(quantity_str), Some(color)) = (parts.next(), parts.next()) {
+                        if let Ok(quantity) = quantity_str.parse::<u32>() {
+                            return Some((color, quantity));
+                        }
+                    }
+                    None
                 })
             })
         })
+        .filter_map(|x| x)
         .collect();
 
     (id_, cubes)
@@ -104,7 +103,7 @@ fn parse_bag(line: &mut str) -> (u32, Vec<(&str, u32)>) {
 
 fn get_inputs(path: &str) -> Vec<String> {
     let buffer = BufReader::new(File::open(path).expect("Failed to open file"));
-    buffer.lines().map(move |l| l.unwrap()).collect()
+    buffer.lines().map(|l| l.unwrap()).collect()
 }
 
 fn get_args() -> (option, filename) {
